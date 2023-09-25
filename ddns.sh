9b7ef17aeaa98466460ce6577042de28596e3a2c
RexLee <62170324+RexLee0929@users.noreply.github.com>
2023-09-25 17:45:19 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
+    red(){
+        echo -e "\033[31m\033[01m$1\033[0m"
+    }
+    ## 常规用绿色
+    green(){
+        echo -e "\033[32m\033[01m$1\033[0m"
+    }
+    ## 标题用黄色
+    yellow(){
+        echo -e "\033[33m\033[01m$1\033[0m"
+    }
+    ## 提示用蓝色
+    blue(){
+        echo -e "\033[34m\033[01m$1\033[0m"
+    }
+    ## 重要提示用橙色
+    orange(){
+        echo -e "\033[38;5;208m\033[01m$1\033[0m"
+    }
+    
 # 开启严格模式
 set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "=== 开始运行DDNS更新脚本 ==="
+echo
+yellow " === 开始运行DDNS更新脚本 === "
 
 # 初始化默认参数
-echo "初始化默认参数..."
+echo 
+green " 初始化默认参数 " 
 CFKEY=""
 CFUSER=""
 CFZONE_NAME=""
@@ -24,7 +46,8 @@ if [ "$CFRECORD_TYPE" = "AAAA" ]; then
   WANIPSITE="http://ipv6.icanhazip.com"
 fi
 
-echo "正在解析参数..."
+echo 
+green " 正在解析参数 "
 # 解析命令行参数
 while getopts k:u:h:z:t:f:p: opts; do
   case ${opts} in
@@ -39,23 +62,28 @@ while getopts k:u:h:z:t:f:p: opts; do
 done
 
 # 获取当前WAN IP
-echo "获取当前WAN IP..."
+echo
+green " 获取当前 WAN IP "
 WAN_IP=$(curl -s ${WANIPSITE})
-echo "当前WAN IP: $WAN_IP"
+echo 
+blue " 当前 WAN IP : $WAN_IP "
 
 # 定义WAN IP文件位置
 WAN_IP_FILE=$HOME/.cf-wan_${CFRECORD_TYPE}_$CFRECORD_NAME.txt
 
 # 获取Cloudflare Zone ID
-echo "获取Cloudflare Zone ID..."
+echo
+green " 获取 Cloudflare Zone ID "
 CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" \
   -H "X-Auth-Email: $CFUSER" \
   -H "X-Auth-Key: $CFKEY" \
   -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 ) || { echo "获取Zone ID失败"; exit 1; }
-echo "CFZONE_ID: $CFZONE_ID"
+echo 
+blue "CFZONE_ID: $CFZONE_ID"
 
 # 获取DNS记录
-echo "获取DNS记录..."
+echo 
+green " 获取DNS记录 "
 DNS_RECORDS_JSON=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?type=$CFRECORD_TYPE&name=$CFRECORD_NAME" \
   -H "X-Auth-Email: $CFUSER" \
   -H "X-Auth-Key: $CFKEY" \
@@ -68,15 +96,18 @@ EXISTING_PROXY=$(echo $DNS_RECORDS_JSON | grep -Po '(?<="proxied":)[^,]*' | head
 
 # 打印调试信息
 # echo "Debug: Full API Response for DNS Records: $DNS_RECORDS_JSON"
-echo " DNS 记录 ID = $CFRECORD_ID"
-echo " DSN 记录 IP = $EXISTING_IP"
-echo " DSN 记录代理状态 = $EXISTING_PROXY"
+echo 
+blue " DNS 记录 ID = $CFRECORD_ID "
+blue " DSN 记录 IP = $EXISTING_IP "
+blue " DSN 记录代理状态 = $EXISTING_PROXY "
 
 # 检查是否需要更新或创建记录
 if [ -n "$CFRECORD_ID" ]; then
-    echo "找到了记录，检查是否需要更新..."
+    echo
+    green " 找到了记录，检查是否需要更新 "
     if [ "$EXISTING_IP" != "$WAN_IP" ] || [ "$EXISTING_PROXY" != "$CFPROXY" ]; then
-        echo "IP地址或代理状态有变化，正在更新..."
+        echo
+        green " IP地址或代理状态有变化，正在更新 "
         # 更新记录
         RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
           -H "X-Auth-Email: $CFUSER" \
@@ -86,17 +117,21 @@ if [ -n "$CFRECORD_ID" ]; then
         
         # 检查操作是否成功
         if [[ "$RESPONSE" == *'"success":true'* ]]; then
-          echo "更新记录成功！"
+          echo 
+          orange " 更新记录成功! "
         else
-          echo '更新记录失败 :('
-          echo "Response: $RESPONSE"
+          echo 
+          red ' 更新记录失败 : ( '
+          red " Response : $RESPONSE " 
           exit 1
         fi
     else
-        echo "IP地址和代理状态没有变化，无需更新。"
+        echo
+        orange " IP 和 Proxy 状态没有变化，无需更新 "
     fi
 else
-    echo "没有找到记录，正在创建新记录..."
+    echo 
+    green " 没有找到记录，正在创建新记录 "
     # 创建记录
     RESPONSE=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records" \
       -H "X-Auth-Email: $CFUSER" \
@@ -106,12 +141,15 @@ else
     
     # 检查操作是否成功
     if [[ "$RESPONSE" == *'"success":true'* ]]; then
-      echo "创建记录成功！"
+      echo 
+      orange " 创建记录成功! "
     else
-      echo '创建记录失败 :('
-      echo "Response: $RESPONSE"
+      echo
+      red ' 创建记录失败 : ( '
+      red " Response : $RESPONSE "
       exit 1
     fi
 fi
 
-echo "=== DDNS更新脚本运行完毕 ==="
+echo
+yellow " === DDNS更新脚本运行完毕 === "
