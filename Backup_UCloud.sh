d91e7b2c7893cf9151c033bdd81100aa536aaf15
RexLee <62170324+RexLee0929@users.noreply.github.com>
2023-09-28 23:03:04 +0800

    Create Backup_UCloud.sh

---
+++ Backup_UCloud.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 获取当前时间，格式如 2023092900
+current_time=$(date +"%Y%m%d%H%M")
+
+# 指定源文件和备份目录
+src_file="/etc/caddy/Caddyfile"
+backup_dir="/Backup"
+
+# 创建备份目录，如果不存在
+mkdir -p "$backup_dir"
+
+# 使用当前时间作为前缀，复制文件到备份目录
+cp "$src_file" "$backup_dir/caddy/${current_time}_Caddyfile"
