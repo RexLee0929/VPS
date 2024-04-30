660f2c062d7210ca7541dd9733650ea6f9f63c01
RexLee <62170324+RexLee0929@users.noreply.github.com>
2024-04-30 13:24:10 +0800

    Update nnr.sh

--- sh/nnr.sh
+++ sh/nnr.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-
+env_help() {
+	echo " Hello,初次使用请准备一个 $env_name 文件, 内容如下: "
+	echo " cf_key=\"\" # Cloudflare API "
+	echo " cf_email=\"\" # Cloudflare 邮箱 "
+	echo " nnr_token=\"\" # nnr token "
+	echo " nnr_url=\"https://nnr.moe\" # nnr 网址"
+	echo " domain=\"\" # 用于 DNS 解析的域名 "
+	echo " 具体 name prefix ip 后的数字根据你要ddns的规则数量来决定 "
+	echo " name1=\"安徽CM-香港\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " name2=\"安徽CU-香港 x2\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " name3=\"广州CM-香港 x1.5\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " name4=\"广港IEPL1 x10\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " prefix1=\"nnr-ag-cm\" # 解析的域名前缀 "
+	echo " prefix2=\"nnr-ag-cu\" # 解析的域名前缀 "
+	echo " prefix3=\"nnr-gg\" # 解析的域名前缀 "
+	echo " prefix4=\"nnr-gg-iepl\" # 解析的域名前缀 "
+	echo " ip1=\"\" # 留空 "
+	echo " ip2=\"\" # 留空 "
+	echo " ip3=\"\" # 留空 "
+	echo " ip4=\"\" # 留空 "
+}
 # 初始化变量
 env_file="vps.env"
 env_name="vps.env"
@@ -9,7 +29,7 @@ cf_key=""
 cf_email=""
 domain=""
 
-# 使用getopts解析命名参数
+# 解析命名参数
 while getopts "f:t:u:k:e:d:c:" opt; do
   case $opt in
     f) env_file="$OPTARG";;
@@ -23,21 +43,24 @@ while getopts "f:t:u:k:e:d:c:" opt; do
   esac
 done
 
-# 函数用于判断是否为URL
+# 判断是否为URL
 is_url() {
   if [[ $1 =~ ^https?:// ]]; then
-    return 0  # 是URL
+    return 0
   else
-    return 1  # 不是URL
+    return 1
   fi
 }
 
-# 根据env_file变量的值来决定操作
+# 根据 env_file 值来决定操作
 if is_url "$env_file"; then
   echo "从URL下载env文件：$env_file"
   curl -L "$env_file" -o "$env_name"
 elif [ ! -f "$env_file" ]; then
-  echo "未找到env文件。请输入文件地址以下载："
+  echo "未找到env文件"
+  echo "env文件格式如下:"
+  env_help
+  echo "请输入文件地址以下载："
   read -r file_url
   if is_url "$file_url"; then
     curl -L "$file_url" -o "$env_name"
@@ -47,26 +70,13 @@ elif [ ! -f "$env_file" ]; then
   fi
 fi
 
-# # 读取环境变量文件
-# if [ -f "$env_file" ]; then
-#   source "$env_file"
-# else
-#   echo "未找到环境变量文件：$env_file"
-#   exit 1
-# fi
-
 # 读取环境变量文件
-declare -A env_vars
-while IFS= read -r line || [[ -n "$line" ]]; do
-  if [[ $line =~ ^[a-zA-Z_]+[a-zA-Z0-9_]*=.*$ ]]; then
-    key="${line%%=*}"
-    value="${line#*=}"
-    # 删除可能的引号
-    value="${value%\"}"
-    value="${value#\"}"
-    env_vars[$key]="$value"
-  fi
-done < "$env_name"
+if [ -f "$env_name" ]; then
+  source "$env_name"
+else
+  echo "未找到环境变量文件：$env_name"
+  exit 1
+fi
 
 # 打印环境变量和传入值
 print_vars() {
@@ -76,6 +86,10 @@ print_vars() {
     echo "cf_key: $cf_key"
     echo "cf_email: $cf_email"
     echo "domain: $domain"
+    echo "env_file: $env_file"
+    echo "env_file: $env_file"
+    echo "env_file: $env_file"
+    echo "env_file: $env_file"
     echo "env_file: $env_file"
 }
 
@@ -170,6 +184,29 @@ ddns_update() {
     fi
 }
 
+
+env_help() {
+	echo " Hello,初次使用请准备一个 $env_name 文件, 内容如下: "
+	echo " cf_key=\"\" # Cloudflare API "
+	echo " cf_email=\"\" # Cloudflare 邮箱 "
+	echo " nnr_token=\"\" # nnr token "
+	echo " nnr_url=\"https://nnr.moe\" # nnr 网址"
+	echo " domain=\"\" # 用于 DNS 解析的域名 "
+	echo " 具体 name prefix ip 后的数字根据你要ddns的规则数量来决定 "
+	echo " name1=\"安徽CM-香港\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " name2=\"安徽CU-香港 x2\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " name3=\"广州CM-香港 x1.5\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " name4=\"广港IEPL1 x10\" # 创建规则时自动创建的备注 \" ->\" 前的名称 不要留空格 "
+	echo " prefix1=\"nnr-ag-cm\" # 解析的域名前缀 "
+	echo " prefix2=\"nnr-ag-cu\" # 解析的域名前缀 "
+	echo " prefix3=\"nnr-gg\" # 解析的域名前缀 "
+	echo " prefix4=\"nnr-gg-iepl\" # 解析的域名前缀 "
+	echo " ip1=\"\" # 留空 "
+	echo " ip2=\"\" # 留空 "
+	echo " ip3=\"\" # 留空 "
+	echo " ip4=\"\" # 留空 "
+}
+
 # 打印环境变量和传入值
 print_vars
 
@@ -199,5 +236,4 @@ case $choice in
     *)
         echo " 无效的选择  "
         sleep 2s
-    esac
 esac
